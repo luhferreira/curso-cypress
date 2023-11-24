8e3320ad85939e22ad1e943024530e12e04f2c25
Luana Ferreira <56826627+luhferreira@users.noreply.github.com>
2023-11-24 11:00:52 -0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
-  
+  cy.get('#firstName').type('Luana')
+  cy.get('#lastName').type('Ferreira')
+  cy.get('#email').type('luhferreira@exemplo.com')
+  cy.get('#open-tex-area').type('Test')
+  cy.get('button[type="submit"]').click()
 })
